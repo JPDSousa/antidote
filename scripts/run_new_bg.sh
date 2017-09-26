49c5c7e5e3e775229a43230b88058b1d32b65b24
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-26 14:55:59 +0100

    create log dir only if it doesn't exist

--- scripts/run_new_bg.sh
+++ scripts/run_new_bg.sh
@@ -3,6 +3,6 @@
 MY_IP=$(curl v4.ifconfig.co)
 
 export NODE_NAME="antidote@$MY_IP"
-mkdir log
+mkdir -p log
 touch ./log/console.log
 ./_build/default/rel/antidote/bin/env start && sleep 10 && tail -f ./log/console.log &
